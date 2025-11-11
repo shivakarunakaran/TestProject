82acb3904f65d8f58454024bf8f294db36ac7a35
Shiva Karunakaran <letter2karan@gmail.com>
2025-11-11 18:16:27 +0530

    enter

--- src/test/java/basecls/Browse.java
+++ src/test/java/basecls/Browse.java
@@ -2,7 +2,7 @@ package basecls;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
+//import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeTest;
 
 public class Browse 
